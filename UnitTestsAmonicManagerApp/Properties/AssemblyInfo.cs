3131ae3802628ee030b3011ee17d892774405112
Volodimir <you@example.com>
2024-07-29 19:06:16 +0300

    Tests

---
+++ UnitTestsAmonicManagerApp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestsAmonicManagerApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestsAmonicManagerApp")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f00ed832-af52-4f72-b088-7adfc68c38c7")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestsAmonicManagerApp/StatisticTests.cs
@@ -0,0 +1,23 @@
+﻿using AmonicManagerApp.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestsAmonicManagerApp
+{
+    [TestClass]
+    public class StatisticTests
+    {
+        [TestMethod]
+        public void TestFlyingCount_Execute()
+        {
+            int result = StartViewModel.ViewModel.FlyingCount;
+            Assert.AreEqual(result, 15);
+        }
+        [TestMethod]
+        public void TestTicketCount_Execute()
+        {
+            int result = StartViewModel.ViewModel.TicketCount;
+            Assert.AreEqual(result, 5);
+        }
+    }
+}
